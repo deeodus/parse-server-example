495d5d15e082ddeddcd970b46e8a5dd823a6871b
deeodus <22453138+deeodus@users.noreply.github.com>
2017-10-12 15:14:11 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,21 +2,3 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hello this is Dee and ' + req.params.name);
 });
-
-
-Parse.Cloud.define('test', function(request, response) {
-  const query = new Parse.Query('Cases');
-  query.equalTo('caseNo', request.params.caseNo);
-    .find({ useMasterKey: true })
-    .then((results) => {
-      //let sum = 0;
-      //for (let i = 0; i < results.length; ++i) {
-        //sum += results[i].get("stars");
-      //}
-      //response.success(sum / results.length);
-      response.success('Object ID' + results[0].get('objectId'));
-    })
-    .catch(() =>  {
-      response.error('case lookup failed');
-    });
-});
