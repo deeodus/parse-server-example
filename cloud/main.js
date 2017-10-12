2e484a2f8e1de6a3def97edb34bf1ed895eb01c3
deeodus <22453138+deeodus@users.noreply.github.com>
2017-10-12 16:11:18 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,11 +1,11 @@
 
 Parse.Cloud.define("test", function(request, response) {
-  var query = new Parse.Query("Coupon");
+  var query = new Parse.Query("Cases");
   //query.equalTo("movie", request.params.movie);
   query.find().then((results) => {
       let sum = 0;
       for (let i = 0; i < results.length; ++i) {
-        sum += results[i].get("used");
+        sum += results[i].get("price");
       }
       response.success(sum);
     })
