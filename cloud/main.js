6e70c1b911105e81db02ee444d04e5d1e1a8e4bc
deeodus <22453138+deeodus@users.noreply.github.com>
2017-10-12 17:29:57 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,14 @@
 
 Parse.Cloud.define("findCases", function(request, response) {
-  var query = new Parse.Query("Cases");
-  query.equalTo("username", request.params.username);
-  query.find({ useMasterKey: true }).then((results) => {
+  
+  var query = new Parse.Query(Parse.User);
+  query.equalTo("category", "Patient");  // find all the women
+  query.find({
+    success: function(patients) {
+      
+      var query = new Parse.Query("Cases");
+    query.equalTo("username", request.params.username);
+    query.find({ useMasterKey: true }).then((results) => {
       //let sum = 0;
     let newUsername = request.params.newUsername;
       for (let i = 0; i < results.length; ++i) {
@@ -10,15 +16,28 @@ Parse.Cloud.define("findCases", function(request, response) {
         
         let object = results[i];           
         object.set("username",newUsername);
+        
+        var acl = new Parse.ACL();
+        //acl.setRoleWriteAccess("admins", true);
+        acl.setReadAccess(req.user, true);
+        acl.setWriteAccess(req.user, true);
+        object.setACL(acl);
+        
         object.save(null, { useMasterKey: true });
       }
-      
-    response.success("successful change");
+    
+      response.success("success" + patients[0].get('username'));
       //response.success(sum);
     })
     .catch(() =>  {
-      response.error("movie lookup failed");
+      response.error("fail");
     });
+      
+      //=======
+  }
+    
+  });
+  
 });
 
 
