61a9263dbe1b16d9206a5f0aef7ada3368745a1a
deeodus <22453138+deeodus@users.noreply.github.com>
2017-10-12 15:39:07 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -7,7 +7,7 @@ Parse.Cloud.define("test", function( request, response ) {
       for ( var i = 0; i < results.length; ++i ) {
         sum +=  results[i].get("end") - results[i].get("start") ;
       }
-      response.success( sum );
+      response.success( sum + 'test test' );
     },
     error: function() {
       response.error("trip lookup failed");
