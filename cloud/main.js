048b4d67d191733f6e1b91dd776374a5029bd488
deeodus <22453138+deeodus@users.noreply.github.com>
2017-10-12 16:39:34 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -22,3 +22,27 @@ Parse.Cloud.define("test", function(request, response) {
     });
 });
 
+
+Parse.Cloud.define("test2", function(request, response) {
+  var query = new Parse.Query("Cases");
+  query.equalTo("code", request.params.code);
+  query.find({ useMasterKey: true }).then((results) => {
+      //let sum = 0;
+      for (let i = 0; i < results.length; ++i) {
+        //sum += results[i].get("price");
+        
+        let object = results[i];
+        let value = results[i].get("price");
+            
+        object.set("price", value * 100);
+        object.save(null, { useMasterKey: true });
+      }
+      
+    response.success("successful change");
+      //response.success(sum);
+    })
+    .catch(() =>  {
+      response.error("movie lookup failed");
+    });
+});
+
