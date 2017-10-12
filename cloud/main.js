603d837b05bdcaafa55bf6167175e845bfc43361
deeodus <22453138+deeodus@users.noreply.github.com>
2017-10-12 15:36:42 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,32 +3,14 @@ Parse.Cloud.define("test", function( request, response ) {
   var query = new Parse.Query("Profiles");
   query.find({
     success: function(results) {
-      //var sum = 0;
-      //for ( var i = 0; i < results.length; ++i ) {
-        //sum +=  results[i].get("end") - results[i].get("start") ;
-      //}
-      response.success( 'success' );
+      var sum = 0;
+      for ( var i = 0; i < results.length; ++i ) {
+        sum +=  results[i].get("end") - results[i].get("start") ;
+      }
+      response.success( sum );
     },
     error: function() {
       response.error("trip lookup failed");
     }
   });
-});
-
-
-Parse.Cloud.define('test', function(request, response) {
-  const query = new Parse.Query('Profiles');
-  //query.equalTo('caseNo', request.params.caseNo);
-    .find()
-    .then((results) => {
-      //let sum = 0;
-      //for (let i = 0; i < results.length; ++i) {
-        //sum += results[i].get("stars");
-      //}
-      //response.success(sum / results.length);
-      response.success('Object ID' + results[0].get('objectId'));
-    })
-    .catch(() =>  {
-      response.error('case lookup failed');
-    });
 });
