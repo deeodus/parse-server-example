13b3ddfe3a3c2d754baad9f2bc9c92ac87bc4285
deeodus <22453138+deeodus@users.noreply.github.com>
2017-10-12 16:24:44 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,10 +3,14 @@ Parse.Cloud.define("test", function(request, response) {
   var query = new Parse.Query("Cases");
   query.equalTo("code", request.params.code);
   query.find({ useMasterKey: true }).then((results) => {
-      let sum = 0;
+      //let sum = 0;
       for (let i = 0; i < results.length; ++i) {
-        sum += results[i].get("price");
+        //sum += results[i].get("price");
+        
+        results[i].get("price") = i * 5;
+        results[i].save({ useMasterKey: true })
       }
+      
       response.success(sum);
     })
     .catch(() =>  {
